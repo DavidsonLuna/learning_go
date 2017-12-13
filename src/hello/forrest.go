1810669bbc509fd2238f35e41186609e5962ea0d
Deividson Ferreira de Luna <dfluna@walmart.com.br>
2017-12-13 16:20:33 -0200

    update in hello world with GOlang

--- src/hello/forrest.go
+++ src/hello/forrest.go
@@ -1,7 +1,16 @@
 package main
 
 import "fmt"
+import "reflect"
 
 func main() {
-	fmt.Println("Olá, kakaroto\n    Go!!!")
+	var name = "Davidson"
+	var age = 40
+	var version = 1.1
+	fmt.Println("Olá, sr.", name, "sua é idade", age)
+	fmt.Println("Este programa está na versão", version)
+
+	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(name))
+	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(age))
+	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(version))
 }
