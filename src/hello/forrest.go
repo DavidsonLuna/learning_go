419d7bf1ff3b6213f802b60e69b175e6af3cf4cd
Deividson Ferreira de Luna <dfluna@walmart.com.br>
2017-12-14 16:39:05 -0200

    Add new steps of my code

--- src/hello/forrest.go
+++ src/hello/forrest.go
@@ -13,12 +13,33 @@ func main() {
 	fmt.Println("0 - Sair do Programa")
 
 	var command int
-	fmt.Scanf("%d", &command)
-
+	fmt.Scan(&command)
 	fmt.Println("The chosen command was", command)
 
+	//option 1 conditional
+	// if command == 1 {
+	// 	fmt.Println("Monitorando...")
+	// } else if command == 2 {
+	// 	fmt.Println("Exibindo Logs...")
+	// } else if command == 0 {
+	// 	fmt.Println("Saindo do programa...")
+	// } else {
+	// 	fmt.Println("Desculpe, mas não conheço esse comando")
+
+	//option 2 conditional
+	switch command {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+	default:
+		fmt.Println("Desculpe, mas não conheço esse comando")
+	}
+	//Show variables type results
 	// fmt.Println("O tipo da variavel nome é", reflect.TypeOf(name))
 	// fmt.Println("O tipo da variavel nome é", reflect.TypeOf(age))
 	// fmt.Println("O tipo da variavel nome é", reflect.TypeOf(version))
-	fmt.Println("O endereço da minha variavel command", &command)
+	//fmt.Println("O endereço da minha variavel command", &command)
 }
