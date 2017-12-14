d3db2d34201794a1755a0c4279c53e089dc99a50
Deividson Ferreira de Luna <dfluna@walmart.com.br>
2017-12-14 09:05:15 -0200

    Add New commands

--- src/hello/forrest.go
+++ src/hello/forrest.go
@@ -1,16 +1,24 @@
 package main
 
 import "fmt"
-import "reflect"
 
 func main() {
-	var name = "Davidson"
-	var age = 40
-	var version = 1.1
-	fmt.Println("Olá, sr.", name, "sua é idade", age)
+	name := "Forrest"
+	version := 1.1
+	fmt.Println("Olá, sr.", name)
 	fmt.Println("Este programa está na versão", version)
 
-	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(name))
-	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(age))
-	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(version))
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var command int
+	fmt.Scanf("%d", &command)
+
+	fmt.Println("The chosen command was", command)
+
+	// fmt.Println("O tipo da variavel nome é", reflect.TypeOf(name))
+	// fmt.Println("O tipo da variavel nome é", reflect.TypeOf(age))
+	// fmt.Println("O tipo da variavel nome é", reflect.TypeOf(version))
+	fmt.Println("O endereço da minha variavel command", &command)
 }
