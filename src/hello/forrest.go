d154f0be4b59c6c6fdaa45ff5221095d82d4a111
Deividson Ferreira de Luna <dfluna@walmart.com.br>
2017-12-20 09:08:52 -0200

    New update

--- src/hello/forrest.go
+++ src/hello/forrest.go
@@ -3,10 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	name := "Forrest"
-	version := 1.1
-	fmt.Println("Olá, sr.", name)
-	fmt.Println("Este programa está na versão", version)
+
+	showFunction()
 
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
@@ -15,16 +13,6 @@ func main() {
 	var command int
 	fmt.Scan(&command)
 	fmt.Println("The chosen command was", command)
-
-	//option 1 conditional
-	// if command == 1 {
-	// 	fmt.Println("Monitorando...")
-	// } else if command == 2 {
-	// 	fmt.Println("Exibindo Logs...")
-	// } else if command == 0 {
-	// 	fmt.Println("Saindo do programa...")
-	// } else {
-	// 	fmt.Println("Desculpe, mas não conheço esse comando")
 
 	//option 2 conditional
 	switch command {
@@ -37,9 +25,29 @@ func main() {
 	default:
 		fmt.Println("Desculpe, mas não conheço esse comando")
 	}
-	//Show variables type results
-	// fmt.Println("O tipo da variavel nome é", reflect.TypeOf(name))
-	// fmt.Println("O tipo da variavel nome é", reflect.TypeOf(age))
-	// fmt.Println("O tipo da variavel nome é", reflect.TypeOf(version))
-	//fmt.Println("O endereço da minha variavel command", &command)
+
 }
+
+func showFunction() {
+	name := "Forrest"
+	version := 1.1
+	fmt.Println("Olá, sr.", name)
+	fmt.Println("Este programa está na versão", version)
+
+}
+
+//option 1 conditional
+// if command == 1 {
+// 	fmt.Println("Monitorando...")
+// } else if command == 2 {
+// 	fmt.Println("Exibindo Logs...")
+// } else if command == 0 {
+// 	fmt.Println("Saindo do programa...")
+// } else {
+// 	fmt.Println("Desculpe, mas não conheço esse comando")
+
+//Show variables type results
+// fmt.Println("O tipo da variavel nome é", reflect.TypeOf(name))
+// fmt.Println("O tipo da variavel nome é", reflect.TypeOf(age))
+// fmt.Println("O tipo da variavel nome é", reflect.TypeOf(version))
+//fmt.Println("O endereço da minha variavel command", &command)
